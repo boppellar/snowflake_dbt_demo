46a6765b6f8fb6e2a8510f02ad74f4a218305859
Boppella Ravindra Reddy <reddyravindra902@gmail.com>
2026-06-07 13:47:44 +0000

    commit

--- models/staging/stg_customers.sql
+++ models/staging/stg_customers.sql
@@ -1,1 +1,4 @@
-select customer_id,upper(customer_name) as customer_name,upper(city) as city,lower(email) as email from {{ source('raw','customers') }}
+select customer_id,upper(customer_name)
+ as customer_name,upper(city) as city,
+ lower(email_id) as email 
+ from {{ source('raw','customers') }}

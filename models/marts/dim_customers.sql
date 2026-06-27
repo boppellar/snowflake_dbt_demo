16d21f38bf3c6285188b962caafe505fb22ba30e
Boppella Ravindra Reddy <reddyravindra902@gmail.com>
2026-06-27 13:33:04 +0000

    commit

--- models/marts/dim_customers.sql
+++
@@ -1,5 +0,0 @@
-select
-    customer_id,
-    customer_name,
-    city
-from {{ ref('int_customer_transactions') }}
--- models/marts/fct_transactions.sql
+++
@@ -1,6 +0,0 @@
-select
-    transaction_id,
-    account_id,
-    amount,
-    transaction_date
-from {{ ref('int_customer_transactions') }}

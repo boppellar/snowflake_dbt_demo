bde2ef9abcc93e92efb93fe5aeeb325f49ba9388
Boppella Ravindra Reddy <reddyravindra902@gmail.com>
2026-06-28 15:08:37 +0000

    commit

--- models/marts/facts/fact_sales.sql
+++ models/marts/facts/fact_sales.sql
@@ -6,31 +6,35 @@
 
 SELECT
 
-    order_line_id,
+    s.order_line_id,
+    s.order_id,
+    s.order_date,
 
-    order_id,
-    order_date,
+    dc.customer_sk,
+    dp.product_sk,
 
-    customer_id,
+    s.quantity,
+    s.unit_price,
+    s.discount,
+    s.tax_amount,
+    s.line_total,
 
-    product_id,
-
-    quantity,
-    unit_price,
-    discount,
-    tax_amount,
-    line_total,
-
-    payment_method,
-    payment_status,
+    s.payment_method,
+    s.payment_status,
 
     CURRENT_TIMESTAMP() AS load_timestamp
 
-FROM {{ ref('int_sales_enriched') }}
+FROM {{ ref('int_sales_enriched') }} s
+
+INNER JOIN {{ ref('dim_customer') }} dc
+    ON s.customer_id = dc.customer_id
+
+INNER JOIN {{ ref('dim_product') }} dp
+    ON s.product_id = dp.product_id
 
 {% if is_incremental() %}
 
-WHERE order_date >
+WHERE s.order_date >
 (
     SELECT COALESCE(MAX(order_date), '1900-01-01')
     FROM {{ this }}
